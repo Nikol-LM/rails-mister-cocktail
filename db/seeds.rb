746229e0c25bc353616495d1e8a54b872387169e
nikol lamilla <nikol.lamilla@gmail.com>
2019-08-08 17:21:17 +0200

    my mister-cocktail back end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+
+Ingredient.create!(name: "lemon")
+Ingredient.create!(name: "ice")
+Ingredient.create!(name: "mint leaves")
+Ingredient.create!(name: "Southern Comfort")
+Ingredient.create!(name: "Brandy")
+Ingredient.create!(name: "Sugar")
+Ingredient.create!(name: "demerara Sugar")
+Ingredient.create!(name: "Lime juice")
+Ingredient.create!(name: "Rum")
+Ingredient.create!(name: "Lemon juice")
+Ingredient.create!(name: "Ginger")
+Ingredient.create!(name: "Tomato juice")
+Ingredient.create!(name: "Vodka")
+Ingredient.create!(name: "Gin")
+
+puts 'Finished!'
